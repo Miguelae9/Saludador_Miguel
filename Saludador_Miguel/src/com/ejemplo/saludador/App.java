a6d7d7fcf86db80dc80addddb9f59017ec5f3bce
Miguel Angel <mpergar0904@g.educaand.es>
2025-06-18 12:46:08 +0200

    Commit inicial: clase App con saludo

---
+++ Saludador_Miguel/src/com/ejemplo/saludador/App.java
@@ -0,0 +1,9 @@
+package com.ejemplo.saludador;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, mundo!");
+
+    }
+
+}
---
+++ Saludador_Miguel/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Saludador_Miguel {
+}
