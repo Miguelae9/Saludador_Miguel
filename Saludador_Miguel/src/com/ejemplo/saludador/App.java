c47e503b77b24def6d033241aafdb0a91317e090
Miguel Angel <mpergar0904@g.educaand.es>
2025-06-18 14:25:51 +0200

    Añade parámetro nombre al saludo

--- Saludador_Miguel/src/com/ejemplo/saludador/App.java
+++ Saludador_Miguel/src/com/ejemplo/saludador/App.java
@@ -2,7 +2,9 @@ package com.ejemplo.saludador;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("¡Hola, mundo!");
+    	String nombre = args.length > 0 ? args[0] : "invitado";
+    	System.out.println("¡Hola, " + nombre + "!");
+
 
     }
 
